6052bed435f63ee6e2ea9aa5d283c4a91bcfca5e
Cameron <cameronbrady1527@gmail.com>
2025-03-31 17:18:46 -0400

    removing unused variables

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -66,6 +66,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
     } catch (error) {
         // If a database error occurs, return a more specific error.
         return {
+            errors: error,
             message: 'Database Error: Failed to Create Invoice.'
         }
     }
@@ -103,7 +104,10 @@ export async function updateInvoice(
         WHERE id = ${id}
     `; 
     } catch (error) {
-        return { message: 'Database Error: Failed to Update Invoice.' };
+        return { 
+            errors: error,
+            message: 'Database Error: Failed to Update Invoice.' 
+        };
     }
 
     revalidatePath('/dashboard/invoices');
